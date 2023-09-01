378ea76232f6a5c641a434574f9d652df3efdcf6
HieuTong <hieuminhtongle20111998@gmail.com>
2023-09-01 17:23:37 +0700

    Update main.py modules routers

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from . import models
 from .database import engine
-from routers import post, user, auth, vote
+from .routers import post, user, auth, vote
 
 # models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
@@ -24,3 +24,7 @@ app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
 app.include_router(vote.router)
+
+@app.get("/")
+def root():
+    return {"message": "Hello World"}
