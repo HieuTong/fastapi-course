d713a2db7421adc6133630484b37ac304d92951e
HieuTong <hieuminhtongle20111998@gmail.com>
2023-09-01 17:17:33 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-import models
-from database import engine, Base
+from . import models
+from .database import engine
 from routers import post, user, auth, vote
 
-models.Base.metadata.create_all(bind=engine)
+# models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 origins = [
